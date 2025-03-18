27a6309565d54f7455ad22907f3b3096b1e824df
rainpaw <ethanrbayer@gmail.com>
2025-03-18 07:33:07 -0500

    Added the three heart recipes to one group

--- src/main/java/io/github/rainpaw/lifesteal/items/ItemManager.java
+++ src/main/java/io/github/rainpaw/lifesteal/items/ItemManager.java
@@ -40,18 +40,21 @@ public class ItemManager {
         // Heart Recipes
         ShapedRecipe srKey = new ShapedRecipe(NamespacedKey.minecraft("heart_trial_key"), getHeart());
         srKey.shape("NEN", "ETE", "NEN");
+        srKey.setGroup("heart");
         srKey.setIngredient('N', Material.NAUTILUS_SHELL);
         srKey.setIngredient('E', Material.NETHERITE_INGOT);
         srKey.setIngredient('T', Material.TRIAL_KEY);
 
         ShapedRecipe srDragon = new ShapedRecipe(NamespacedKey.minecraft("heart_dragon_head"), getHeart());
         srDragon.shape("NEN", "EDE", "NEN");
+        srDragon.setGroup("heart");
         srDragon.setIngredient('N', Material.NAUTILUS_SHELL);
         srDragon.setIngredient('E', Material.NETHERITE_INGOT);
         srDragon.setIngredient('D', Material.DRAGON_HEAD);
 
         ShapedRecipe srWither = new ShapedRecipe(NamespacedKey.minecraft("heart_wither_skeleton_skull"), getHeart());
         srWither.shape("NEN", "EWE", "NEN");
+        srWither.setGroup("heart");
         srWither.setIngredient('N', Material.NAUTILUS_SHELL);
         srWither.setIngredient('E', Material.NETHERITE_INGOT);
         srWither.setIngredient('W', Material.WITHER_SKELETON_SKULL);
